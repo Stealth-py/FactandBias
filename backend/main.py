3c3aebc2a727a3cfb1ae0d29eaa17f307e0968b9
Daniil Orel <facedan123@gmail.com>
2023-06-06 12:05:30 +0600

    feat backend: return whole db

--- backend/main.py
+++ backend/main.py
@@ -49,5 +49,10 @@ async def parse(url: str, db: Session = Depends(get_db))->List[ART]:
 
     return articles
 
+
+@app.get("/db")
+async def parse(db: Session = Depends(get_db)):
+    return {"data": db.query(Article).all()}
+
 if __name__=='__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
